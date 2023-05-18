f4782ff3c895923936f5183a986da78626acf3ac
Amer Varajic <varajicamer@gmail.com>
2023-05-18 16:24:08 +0200

    added NavBar & Logo, have some problems with logo

---
+++ Next.js-Developer-Portfolio-Starter-Code-main/src/components/Logo.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import React from "react";
+import { motion } from "framer-motion";
+
+const MotionLink = motion(Link);
+
+const Logo = () => {
+  return (
+    <div className="flex items-center justify-center mt-2">
+      <MotionLink
+        href="/"
+        className="w-16 h-16 bg-dark text-light flex items-center justify-center rounded-full text-2x1 font-bold"
+      >
+        AV
+      </MotionLink>
+    </div>
+  );
+};
+
+export default Logo;
