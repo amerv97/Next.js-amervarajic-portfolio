c94c3ace155080c9c7c4fa82687ec401653d0f0d
Amer Varajic <varajicamer@gmail.com>
2023-05-19 16:13:09 +0200

    finally finished styling for span element

--- Next.js-Developer-Portfolio-Starter-Code-main/src/components/NavBar.js
+++ Next.js-Developer-Portfolio-Starter-Code-main/src/components/NavBar.js
@@ -1,13 +1,19 @@
 import Link from "next/link";
 import React from "react";
 import Logo from "./Logo";
+import { useRouter } from "next/router";
 
 const CustomLink = ({ href, title, className = "" }) => {
+  const router = useRouter();
   return (
     <Link href={href} className={`${className} relative group`}>
       {title}
 
-      <span className="h-[1px] inline-block w-0 bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300">
+      <span
+        className={`h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${
+          router.asPath === href ? "w-full" : "w-0"
+        }`}
+      >
         &nbsp;
       </span>
     </Link>
