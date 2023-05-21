42e476576c9c34def21de3eec098aedb970a17bd
Amer Varajic <varajicamer@gmail.com>
2023-05-21 15:50:45 +0200

    added 2 buttond for CV & contact, styled them

--- Next.js-Developer-Portfolio-Starter-Code-main/src/pages/index.js
+++ Next.js-Developer-Portfolio-Starter-Code-main/src/pages/index.js
@@ -3,6 +3,8 @@ import Head from "next/head";
 import Image from "next/image";
 import profilePic from "../../public/images/profile/developer-pic-1.png";
 import AnimatedText from "@/components/AnimatedText";
+import Link from "next/link";
+import { LinkArrow } from "@/components/Icons";
 
 export default function Home() {
   return (
@@ -23,13 +25,27 @@ export default function Home() {
             </div>
             <div className="w-1/2 flex flex-col self-center items-center">
               <h1> </h1>
-              <AnimatedText text="Turning Vision Into Reality With Code And Design." className="!text-6xl !text-left" />
-              <p>
+              <AnimatedText
+                text="Turning Vision Into Reality With Code And Design."
+                className="!text-6xl !text-left"
+              />
+              <p className="my-4 text-base font-medium">
                 As a skilled full-stack developer, I am dedicated to turning
                 ideas into innovative web applications. Explore my latest
                 projects and articles, showcasing my expertise in React.js and
                 web development.
               </p>
+              <div className="flex items-center self-start mt-2">
+                <Link
+                  href="/dummy.pdf"
+                  target={"_blank"}
+                  className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark"
+                  download={true}
+                >
+                  Resume <LinkArrow className={"w-6 ml-1"} />
+                </Link>
+                <Link href="mailto:varajicamer@gmail.com" className="ml-4 text-lg font-medium capitalize text-dark underline">Contact</Link>
+              </div>
             </div>
           </div>
         </Layout>
