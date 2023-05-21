64cf90dd08d733ccfd527dc837c00f295530224c
Amer Varajic <varajicamer@gmail.com>
2023-05-21 16:31:44 +0200

    created, styled & finished footer

--- Next.js-Developer-Portfolio-Starter-Code-main/src/pages/_app.js
+++ Next.js-Developer-Portfolio-Starter-Code-main/src/pages/_app.js
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import NavBar from "@/components/NavBar";
 import "@/styles/globals.css";
 import { Montserrat } from "next/font/google";
@@ -18,10 +19,10 @@ export default function App({ Component, pageProps }) {
       <main
         className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}
       >
-        <NavBar/>
+        <NavBar />
         <Component {...pageProps} />
+        <Footer />
       </main>
     </>
   );
 }
-
