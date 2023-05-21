4f63555da2c1bd92a00e4c13b0c609f91e8d56ba
Amer Varajic <varajicamer@gmail.com>
2023-05-21 23:25:05 +0200

    imported new icon, new HireMe comp imported on idx

---
+++ Next.js-Developer-Portfolio-Starter-Code-main/src/components/HireMe.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import AmerLogo from './Icons'
+
+
+const HireMe = () => {
+  return (
+    <div className='fixed left-4 bottom-4 flex justify-center overflow-hidden'>
+        <div className='flex w-48 h-auto justify-center relative items-center'>
+        <AmerLogo className={"fill-dark animate-spin-slow"}/>
+        </div>
+
+    </div>
+  )
+}
+
+export default HireMe
