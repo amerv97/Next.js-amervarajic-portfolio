bde995a69a83b045b96366df98da2da84d95fe86
Amer Varajic <varajicamer@gmail.com>
2023-05-22 14:04:17 +0200

    added 3 more details, need to change later

--- Next.js-Developer-Portfolio-Starter-Code-main/src/components/Experience.js
+++ Next.js-Developer-Portfolio-Starter-Code-main/src/components/Experience.js
@@ -43,6 +43,36 @@ const Experience = () => {
                 search engine, including improving the accuracy and relevance of search results and 
                 developing new tools for data analysis and visualization."
             />
+            <Details
+              position="Software Engineer"
+              company="Google"
+              companyLink="www.google.com"
+              time="2022-Present"
+              addres="Mountain View, CA"
+              work="Worked on a team responsible for developing new features for Google's 
+                search engine, including improving the accuracy and relevance of search results and 
+                developing new tools for data analysis and visualization."
+            />
+            <Details
+              position="Software Engineer"
+              company="Google"
+              companyLink="www.google.com"
+              time="2022-Present"
+              addres="Mountain View, CA"
+              work="Worked on a team responsible for developing new features for Google's 
+                search engine, including improving the accuracy and relevance of search results and 
+                developing new tools for data analysis and visualization."
+            />
+            <Details
+              position="Software Engineer"
+              company="Google"
+              companyLink="www.google.com"
+              time="2022-Present"
+              addres="Mountain View, CA"
+              work="Worked on a team responsible for developing new features for Google's 
+                search engine, including improving the accuracy and relevance of search results and 
+                developing new tools for data analysis and visualization."
+            />
           </ul>
         </div>
       </div>
