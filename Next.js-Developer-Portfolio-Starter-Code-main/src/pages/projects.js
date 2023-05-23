10faffd11cb74d8d3c8b28b4fa15207d1743592a
Amer Varajic <varajicamer@gmail.com>
2023-05-23 13:28:57 +0200

    reused created functions

--- Next.js-Developer-Portfolio-Starter-Code-main/src/pages/projects.js
+++ Next.js-Developer-Portfolio-Starter-Code-main/src/pages/projects.js
@@ -124,9 +124,54 @@ const projects = () => {
                 type="Featured Project"
               />{" "}
             </div>
-            <div className="col-span-6">Project 2</div>
-            <div className="col-span-6">Project 3</div>
-            <div className="col-span-6">Project 4</div>
+            <div className="col-span-6">
+              <Project
+                title="Crypto Screener Application"
+                img={project1}
+                summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
+                It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
+                local currency."
+                link="/"
+                github="/"
+                type="Featured Project"
+              />
+            </div>
+            <div className="col-span-12">
+              <FeaturedProject
+                title="Crypto Screener Application"
+                img={project1}
+                summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
+                It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
+                local currency."
+                link="/"
+                github="/"
+                type="Featured Project"
+              />
+            </div>
+            <div className="col-span-6">
+              <Project
+                title="Crypto Screener Application"
+                img={project1}
+                summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
+                It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
+                local currency."
+                link="/"
+                github="/"
+                type="Featured Project"
+              />
+            </div>
+            <div className="col-span-6">
+              <Project
+                title="Crypto Screener Application"
+                img={project1}
+                summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
+                It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
+                local currency."
+                link="/"
+                github="/"
+                type="Featured Project"
+              />
+            </div>
           </div>
         </Layout>
       </main>
