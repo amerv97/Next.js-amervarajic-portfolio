ff8ca3d3146e42c93be5f7c5c02f7f0a13cdd3b7
Amer Varajic <varajicamer@gmail.com>
2023-05-23 14:50:13 +0200

    created article comp & structure

---
+++ Next.js-Developer-Portfolio-Starter-Code-main/src/pages/articles.js
@@ -0,0 +1,59 @@
+import AnimatedText from "@/components/AnimatedText";
+import Layout from "@/components/Layout";
+
+import article1 from "../../public/images//articles/pagination component in reactjs.jpg";
+
+import React from "react";
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+
+const FeaturedArtice = ({ img, title, time, summary, link }) => {
+  return (
+    <>
+      <li>
+        <Link
+          href={link}
+          target="_blank"
+          className="w-full cursor-pointer overflow-hidden rounded-lg"
+        >
+          <Image src={img} alt={title} className="w-full h-auto"></Image>
+        </Link>
+        <Link href={link} target="_blank">
+          <h2 className="text-2xl capitalize font-bold my-2">{title}</h2>
+          <p className="text-sm mb-2">{summary}</p>
+          <span className="text-primary font-semibold">{time}</span>
+        </Link>
+      </li>
+    </>
+  );
+};
+
+const article = () => {
+  return (
+    <>
+      <Head>
+        <title>Amer Varajic | Articles Page</title>
+        <meta name="description" content="description" />
+      </Head>
+      <main className="w-full mb-16 flex flex-col items-center justify-center overflow-hidden">
+        <Layout className="pt-16">
+          <AnimatedText text="Words Can Change The World!" className="mb-16 " />
+          <ul className="grid grid-col-2 gap-16">
+              <FeaturedArtice
+                title="Build A Custom Pagination Component In Reactjs From Scratch"
+                summary="Learn how to build a custom pagination component in ReactJS from scratch. 
+                Follow this step-by-step guide to integrate Pagination component in your ReactJS project."
+                time="9 min read"
+                link="/"
+                img={article1}
+              />
+            <li>Featured article-2</li>
+          </ul>
+        </Layout>
+      </main>
+    </>
+  );
+};
+
+export default article;
