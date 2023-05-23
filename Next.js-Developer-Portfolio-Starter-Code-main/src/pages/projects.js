a80b127e0936e8a7eb1621352d1fcfb2fcb05cd2
Amer Varajic <varajicamer@gmail.com>
2023-05-23 13:22:01 +0200

    project function  finished & styled

--- Next.js-Developer-Portfolio-Starter-Code-main/src/pages/projects.js
+++ Next.js-Developer-Portfolio-Starter-Code-main/src/pages/projects.js
@@ -47,6 +47,43 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
   );
 };
 
+const Project = ({ title, type, img, link, github }) => {
+  return (
+    <article className="w-full flex flex-col items-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative">
+      <Link
+        href={link}
+        target="_blank"
+        className="w-full cursor-pointer overflow-hidden rounded-lg"
+      >
+        <Image src={img} alt={title} className="w-full h-auto"></Image>
+      </Link>
+
+      <div className="w-full flex flex-col items-start justify-between mt-4">
+        <span className="text-primary font-medium text-xl ">{type}</span>
+        <Link
+          href={link}
+          target="_blank"
+          className="hover:underline underline-offset-2"
+        >
+          <h2 className="my-2 w-full text-left text-3xl font-bold ">{title}</h2>
+        </Link>
+        <div className="w-full mt-2 flex items-center justify-between">
+          <Link
+            href={link}
+            target="_blank"
+            className="text-lg font-semibold underline"
+          >
+            Visit
+          </Link>
+          <Link href={github} target="_blank" className="w-8">
+            <GithubIcon />
+          </Link>
+        </div>
+      </div>
+    </article>
+  );
+};
+
 const projects = () => {
   return (
     <>
@@ -57,7 +94,10 @@ const projects = () => {
 
       <main className="w-full mb-16 flex flex-col items-center justify-center">
         <Layout className="pt-16 ">
-          <AnimatedText className="mb-16 " text="Imagination Trumps Knowledge!" />
+          <AnimatedText
+            className="mb-16 "
+            text="Imagination Trumps Knowledge!"
+          />
 
           <div className="grid grid-cols-12 gap-24">
             <div className="col-span-12">
@@ -65,14 +105,25 @@ const projects = () => {
                 title="Crypto Screener Application"
                 img={project1}
                 summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
-                        It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
-                        local currency."
+                It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
+                local currency."
                 link="/"
                 github="/"
                 type="Featured Project"
               />
             </div>
-            <div className="col-span-6">Project 1</div>
+            <div className="col-span-6">
+              <Project
+                title="Crypto Screener Application"
+                img={project1}
+                summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
+                It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
+                local currency."
+                link="/"
+                github="/"
+                type="Featured Project"
+              />{" "}
+            </div>
             <div className="col-span-6">Project 2</div>
             <div className="col-span-6">Project 3</div>
             <div className="col-span-6">Project 4</div>
