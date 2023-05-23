44eaef752b4a05e3963d8b02a40062119710541b
Amer Varajic <varajicamer@gmail.com>
2023-05-23 12:59:31 +0200

    styled FeaturedProject finished

--- Next.js-Developer-Portfolio-Starter-Code-main/src/pages/projects.js
+++ Next.js-Developer-Portfolio-Starter-Code-main/src/pages/projects.js
@@ -11,22 +11,34 @@ import Image from "next/image";
 
 const FeaturedProject = ({ type, title, summary, img, link, github }) => {
   return (
-    <article>
-      <Link href={link} target="_blank">
+    <article className="flex w-full items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12">
+      <Link
+        href={link}
+        target="_blank"
+        className="w-1/2 cursor-pointer overflow-hidden rounded-lg"
+      >
         <Image src={img} alt={title} className="w-full h-auto"></Image>
       </Link>
 
-      <div>
-        <span>{type}</span>
-        <Link href={link} target="_blank">
-          <h2>{title}</h2>
+      <div className="w-1/2 flex flex-col items-start justify-between pl-6">
+        <span className="text-primary font-medium text-xl ">{type}</span>
+        <Link
+          href={link}
+          target="_blank"
+          className="hover:underline underline-offset-2"
+        >
+          <h2 className="my-2 w-full text-left text-4xl font-bold ">{title}</h2>
         </Link>
-        <p>{summary}</p>
-        <div>
-          <Link href={github} target="_blank">
+        <p className="my-2 font-medium text-dark">{summary}</p>
+        <div className="mt-2 flex items-center">
+          <Link href={github} target="_blank" className="w-10">
             <GithubIcon />
           </Link>
-          <Link href={link} target="_blank">
+          <Link
+            href={link}
+            target="_blank"
+            className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold"
+          >
             Visit Project
           </Link>
         </div>
@@ -45,7 +57,7 @@ const projects = () => {
 
       <main className="w-full mb-16 flex flex-col items-center justify-center">
         <Layout className="pt-16 ">
-          <AnimatedText text="Imagination Trumps Knowledge!" />
+          <AnimatedText className="mb-16 " text="Imagination Trumps Knowledge!" />
 
           <div className="grid grid-cols-12 gap-24">
             <div className="col-span-12">
