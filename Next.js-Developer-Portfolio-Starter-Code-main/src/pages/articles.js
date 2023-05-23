b20ecd3b5a9ca80201a250e11a0e939ab85270f9
Amer Varajic <varajicamer@gmail.com>
2023-05-23 15:00:54 +0200

    reused our FeaturedArticle const & styled more..

--- Next.js-Developer-Portfolio-Starter-Code-main/src/pages/articles.js
+++ Next.js-Developer-Portfolio-Starter-Code-main/src/pages/articles.js
@@ -11,7 +11,7 @@ import Image from "next/image";
 const FeaturedArtice = ({ img, title, time, summary, link }) => {
   return (
     <>
-      <li>
+      <li className="col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl">
         <Link
           href={link}
           target="_blank"
@@ -20,7 +20,9 @@ const FeaturedArtice = ({ img, title, time, summary, link }) => {
           <Image src={img} alt={title} className="w-full h-auto"></Image>
         </Link>
         <Link href={link} target="_blank">
-          <h2 className="text-2xl capitalize font-bold my-2">{title}</h2>
+          <h2 className="text-2xl capitalize font-bold my-2 hover:underline">
+            {title}
+          </h2>
           <p className="text-sm mb-2">{summary}</p>
           <span className="text-primary font-semibold">{time}</span>
         </Link>
@@ -39,7 +41,7 @@ const article = () => {
       <main className="w-full mb-16 flex flex-col items-center justify-center overflow-hidden">
         <Layout className="pt-16">
           <AnimatedText text="Words Can Change The World!" className="mb-16 " />
-          <ul className="grid grid-col-2 gap-16">
+          <ul className="grid grid-cols-2 gap-16">
             <FeaturedArtice
               title="Build A Custom Pagination Component In Reactjs From Scratch"
               summary="Learn how to build a custom pagination component in ReactJS from scratch. 
@@ -48,7 +50,14 @@ const article = () => {
               link="/"
               img={article1}
             />
-            <li>Featured article-2</li>
+            <FeaturedArtice
+              title="Build A Custom Pagination Component In Reactjs From Scratch"
+              summary="Learn how to build a custom pagination component in ReactJS from scratch. 
+                Follow this step-by-step guide to integrate Pagination component in your ReactJS project."
+              time="9 min read"
+              link="/"
+              img={article1}
+            />
           </ul>
         </Layout>
       </main>
