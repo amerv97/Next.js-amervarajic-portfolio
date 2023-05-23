b76dab73b75528ba0adc4fd1eebd2558047d403a
Amer Varajic <varajicamer@gmail.com>
2023-05-23 15:18:19 +0200

    added background div border

--- Next.js-Developer-Portfolio-Starter-Code-main/src/pages/articles.js
+++ Next.js-Developer-Portfolio-Starter-Code-main/src/pages/articles.js
@@ -15,15 +15,19 @@ const FramerImage = motion(Image);
 const FeaturedArtice = ({ img, title, time, summary, link }) => {
   return (
     <>
-      <li className="col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl">
+      <li className="relative col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl">
+        <div className="top-0 absolute -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl " />
         <Link
           href={link}
           target="_blank"
           className="w-full inline-block cursor-pointer overflow-hidden rounded-lg"
         >
-          <FramerImage src={img} alt={title} className="w-full h-auto"
-          whileHover={{scale:1.05}}
-          transition={{duration:0.2}}
+          <FramerImage
+            src={img}
+            alt={title}
+            className="w-full h-auto"
+            whileHover={{ scale: 1.05 }}
+            transition={{ duration: 0.2 }}
           />
         </Link>
         <Link href={link} target="_blank">
