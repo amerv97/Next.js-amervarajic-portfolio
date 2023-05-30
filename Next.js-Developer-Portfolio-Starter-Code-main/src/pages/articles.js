3d5652b57da6bcf50812dceda69666db6893c839
Amer Varajic <varajicamer@gmail.com>
2023-05-30 15:15:19 +0200

    article page is done

--- Next.js-Developer-Portfolio-Starter-Code-main/src/pages/articles.js
+++ Next.js-Developer-Portfolio-Starter-Code-main/src/pages/articles.js
@@ -60,7 +60,7 @@ const Article = ({ img, title, date, link }) => {
     <motion.li
       initial={{ y: 200 }}
       whileInView={{ y: 0, transition: { duration: 0.5, ease: "easeInOut" } }}
-      viewport={{once:true}}
+      viewport={{ once: true }}
       className="w-full relative p-4 py-6 my-4 rounded-xl flex items-center  justify-between bg-light text-dark first:mt-0 border border-solid border-dark border-r-4 border-b-4"
     >
       <MovingImg title={title} img={img} link={link} />
