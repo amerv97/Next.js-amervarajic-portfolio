da1b47c76dd75dcde07d0788b55dcddba5fdf289
Amer Varajic <varajicamer@gmail.com>
2023-06-01 10:48:13 +0200

    imporetd responsive parts on tailw. & hambur menu

--- Next.js-Developer-Portfolio-Starter-Code-main/tailwind.config.js
+++ Next.js-Developer-Portfolio-Starter-Code-main/tailwind.config.js
@@ -29,6 +29,24 @@ module.exports = {
           "repeating-radial-gradient(rgba(255,255,255,0.6) 2px,#1b1b1b 8px,#1b1b1b 100px);",
       },
     },
-    plugins: [],
+    screens: {
+      "2xl": { max: "1535px" },
+      // => @media(max-widt: 1535px)
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: "767px" },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
+
+      xs: { max: "479px" },
+      // => @media (max-width: 479px) { ... }
+    },
   },
+  plugins: [],
 };
