2584068a4860ddb1eb3499019113710c01acc29e
Amer Varajic <varajicamer@gmail.com>
2023-06-01 14:20:55 +0200

    hireMe circle pushed in top right corner

--- Next.js-Developer-Portfolio-Starter-Code-main/src/components/HireMe.js
+++ Next.js-Developer-Portfolio-Starter-Code-main/src/components/HireMe.js
@@ -4,8 +4,8 @@ import Link from "next/link";
 
 const HireMe = () => {
   return (
-    <div className="fixed left-4 bottom-4 flex justify-center overflow-hidden">
-      <div className="flex w-48 h-auto justify-center relative items-center">
+    <div className="fixed left-4 bottom-4 flex justify-center overflow-hidden md:right-8 md:left-auto md:top-0 md:bottom-auto">
+      <div className="flex w-48 h-auto justify-center relative items-center md:w-24 md:absolute">
         <AmerLogo className={"fill-dark animate-spin-slow dark:fill-light"} />
 
         <Link
@@ -13,7 +13,9 @@ const HireMe = () => {
           className="flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark text-light shadow-md border border-solid border-dark w-20 h-20 rounded-full font-semibold hover:bg-light hover:text-dark
          
           dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light
-          hover:dark:border-light"
+          hover:dark:border-light
+          
+          md:w-12 md:h-12 md:text-[10px]"
         >
           Hire Me
         </Link>
