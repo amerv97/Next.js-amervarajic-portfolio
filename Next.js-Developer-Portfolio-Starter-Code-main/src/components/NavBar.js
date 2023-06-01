7473fcad165bec13cc14577382ba866b47fd7ddc
Amer Varajic <varajicamer@gmail.com>
2023-06-01 13:20:51 +0200

    added motion  div for mobile version

--- Next.js-Developer-Portfolio-Starter-Code-main/src/components/NavBar.js
+++ Next.js-Developer-Portfolio-Starter-Code-main/src/components/NavBar.js
@@ -162,7 +162,11 @@ const NavBar = () => {
 
       {/* MOBILE */}
       {isOpen ? (
-        <div className="min-w-[70vw] flex flex-col justify-between z-30 items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-32">
+        <motion.div
+          initial={{ scale: 0, opacity: 0, x: "-50%", y: "-50%" }}
+          animate={{ scale: 1, opacity: 1 }}
+          className="min-w-[70vw] flex flex-col justify-between z-30 items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-32"
+        >
           <nav className="flex items-center justify-center flex-col">
             <CustomMobileLink
               href="/"
@@ -250,7 +254,7 @@ const NavBar = () => {
               )}
             </button>
           </nav>
-        </div>
+        </motion.div>
       ) : null}
 
       <div className="left-[50%] absolute top-2 translate-x-[-50]">
