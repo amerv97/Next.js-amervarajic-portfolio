e928a10ca8f9f2920cc10000f9674c8fb76cc3a0
Amer Varajic <varajicamer@gmail.com>
2023-06-01 14:26:55 +0200

    fixed text, resume & contact buttons

--- Next.js-Developer-Portfolio-Starter-Code-main/src/pages/index.js
+++ Next.js-Developer-Portfolio-Starter-Code-main/src/pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
                 text="Turning Vision Into Reality With Code And Design."
                 className="!text-6xl !text-left xl:!text-5xl lg:!text-center lg:!text=6xl md:!text-5xl sm:!text-3xl "
               />
-              <p className="my-4 text-base font-medium">
+              <p className="my-4 text-base font-medium md:text-sm sm:text-xs">
                 As a skilled full-stack developer, I am dedicated to turning
                 ideas into innovative web applications. Explore my latest
                 projects and articles, showcasing my expertise in React.js and
@@ -45,14 +45,17 @@ export default function Home() {
                   target={"_blank"}
                   className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark
                   dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light
-                  hover:dark:border-light"
+                  hover:dark:border-light
+                  
+                  md:p-2 md:px-4 md:text-base"
                   download={true}
                 >
                   Resume <LinkArrow className={"w-6 ml-1"} />
                 </Link>
                 <Link
                   href="mailto:varajicamer@gmail.com"
-                  className="ml-4 text-lg font-medium capitalize text-dark underline dark:text-light"
+                  className="ml-4 text-lg font-medium capitalize text-dark underline dark:text-light
+                  md:text-base"
                 >
                   Contact
                 </Link>
