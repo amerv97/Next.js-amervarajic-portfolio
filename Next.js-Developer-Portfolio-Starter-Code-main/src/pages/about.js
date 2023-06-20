0e14263448e81a6339e92e639340af4b89bb06f9
Amer Varajic <varajicamer@gmail.com>
2023-06-20 16:36:42 +0200

    about section number fixed

--- Next.js-Developer-Portfolio-Starter-Code-main/src/pages/about.js
+++ Next.js-Developer-Portfolio-Starter-Code-main/src/pages/about.js
@@ -99,7 +99,7 @@ const about = () => {
             <div className="col-span-2 flex flex-col justify-between items-end xl:col-span-8 xl:flex-row xl:items-center md:order-1">
               <div className="flex flex-col items-end justify-center xl:items-center">
                 <spam className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl">
-                  <AnimatedNumbers value={100} />+
+                  <AnimatedNumbers value={30} />+
                 </spam>
                 <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm">
                   satisfied clients
@@ -108,7 +108,7 @@ const about = () => {
 
               <div className="flex flex-col items-end justify-center xl:items-center">
                 <spam className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
-                  <AnimatedNumbers value={25} />+
+                  <AnimatedNumbers value={10} />+
                 </spam>
                 <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm">
                   projects completed
